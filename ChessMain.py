9b62bf258063a453105609fd89ef81d35a6a9515
AeroGoob <henrybloxed@gmail.com>
2025-11-04 18:48:35 -0500

    Add imports and initial setup for ChessMain

--- ChessMain.py
+++ ChessMain.py
@@ -1,1 +1,12 @@
 #main file
+import pygame as p
+from Chess import ChessEngine
+
+WIDTH = HEIGHT = 512 
+DIMENSION = 8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15
+IMAGES = {}
+
+def loadImages():
+  IMAGES['wp']
