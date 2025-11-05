32f2b5e1191080590a9ea5526976081031d6d535
AeroGoob <henrybloxed@gmail.com>
2025-11-04 19:39:53 -0500

    Scale chess piece images on load
    Updated image loading to scale chess piece images.

--- ChessMain.py
+++ ChessMain.py
@@ -11,4 +11,10 @@ IMAGES = {}
 def loadImages():
   pieces = ['wp', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ']
   for piece in pieces:
-    IMAGES[piece] = p.image.load("images/" + piece + ".png")
+    IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+def main():
+  p.init()
+  screen = p.display.set_mode((WIDTH, HEIGHT))
+  clock = p.time.Clock()
+  screen.fill(p.Color("white"))
+  gs = ChessEngine.Gamestate()
