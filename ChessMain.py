7ff47073c918788adc57ad37d5e750cd4f972638
AeroGoob <henrybloxed@gmail.com>
2025-11-06 16:58:33 -0500

    Fix syntax error in screen.blit function call

--- ChessMain.py
+++ ChessMain.py
@@ -48,6 +48,6 @@ def drawPieces(screen, board):
     for c in range(DIMENSION): 
       piece = board[r][c]
       if piece != "--": 
-        screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE)
+screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE)
 if __name__ == "__main__":
   main()
